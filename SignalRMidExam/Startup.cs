266e99fe78a29b5812dccfc87ad9fae66f8d185c
IDB _C# <IDB_C#@nh.ctg>
2023-10-11 12:47:36 +0600

    first commit

---
+++ SignalRMidExam/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalRMidExam.Startup))]
+
+namespace SignalRMidExam
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            app.MapSignalR();
+
+        }
+
+    }
+}
